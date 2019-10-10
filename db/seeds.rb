b95309957b8977f4fb4f0f03442d86fa7cea3a61
github username <github email address>
2019-10-10 18:00:22 -0500

    oct 10

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,12 @@
+require_relative '../lib/library_parser.rb'
 # Add seed data here. Seed your database with `rake db:seed`
+# Artist.destroy
+# Song.destroy
+# Genre.destroy
+# SongGenre.destroy
+
+# lib = LibraryParser.new
+# lib.files
+LibraryParser.parse
+
 
